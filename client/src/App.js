2afebbe2c7cea9158ad55a0b36ff5e33bb4866a6
sungminzhao <sungminzhao@gmail.com>
2020-12-28 21:54:40 +0900

    API Sudy

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = theme => ({
@@ -17,6 +18,9 @@ const styles = theme => ({
 },
 table: {
   minWidth: 1080
+},
+progress: {
+  margin: theme.spacing.uint * 2
 }
 })
 
@@ -49,13 +53,15 @@ table: {
 class App extends Component {
 
   state = {
-    customers: ""
+    customers: "",
+    complete: 0
   }
 
   componentDidMount() {
-    this.callApi()
+    this.timer = setInterval(this.progress, 20);
+  /*  this.callApi()
     .then(res => this.setState({customers:res}))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err));*/
   }
 
   callApi = async() => {
@@ -63,6 +69,12 @@ class App extends Component {
     const body = await response.json();
     return body;
   }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -80,12 +92,17 @@ class App extends Component {
        </TableHead>
        <TableBody>
          {this.state.customers ? this.state.customers.map(c => {
-            return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} Birthday={c.Birthday} Gender={c.Gender} Job={c.Job} /> ); }) : ""}
+            return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} Birthday={c.Birthday} Gender={c.Gender} Job={c.Job} /> ); }) :
+       <TableRow>
+          <TableCell colSpan = "6" align = "center">
+            <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+          </TableCell>
+       </TableRow>
+       }
        </TableBody>
         </Table>
     </Paper>
   );
 }
-}
-
+  }
 export default withStyles(styles)(App);
